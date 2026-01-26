53ba7f2124dfe6f50020188610150a594fdfaec7
ksudarshannrcb12-eng <ksudarshan.nrcb.12@gmail.com>
2026-01-26 18:39:45 -0500

    Create Subtraction.cs

---
+++ ToWork/Subtraction.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ArithmeticPractice
+{
+    public class Subtraction
+    {
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
