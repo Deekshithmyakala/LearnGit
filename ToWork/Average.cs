2970d42b6e585480751f1cb9b1e7294a3bde16b7
ksudarshannrcb12-eng <ksudarshan.nrcb.12@gmail.com>
2026-01-26 18:42:03 -0500

    Create Average.cs

---
+++ ToWork/Average.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ArithmeticPractice
+{
+    public class Average
+    {
+        public double CalculateAverage(int a, int b)
+        {
+            return (a + b) / 2.0;
+        }
+    }
+}
