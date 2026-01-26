1d7319688fb412e467f3b68cdb04d71abe58daa2
ksudarshannrcb12-eng <ksudarshan.nrcb.12@gmail.com>
2026-01-26 18:40:11 -0500

    Create Multiplication.cs

---
+++ ToWork/Multiplication.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ArithmeticPractice
+{
+    public class Multiplication
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
